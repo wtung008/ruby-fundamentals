f6c15777337d3bea204658e40a3233663b0ae8ab
wtung008 <wayne.h.tung@gmail.com>
2017-01-16 17:22:39 -0500

    Add exercise2.rb

---
+++ exercise2.rb
@@ -0,0 +1,28 @@
+#1Calculation of a good tip for a $55 meal
+#print "What is the tip amount for $55?"
+#tip = gets.chomp.to_f
+
+
+#if tip >= 8.25
+#  puts "Good tip"
+#else
+#  puts "Bad tip"
+#end
+
+#2 Adding strings with integer
+#a = "Three is "
+#b = 3
+#b=b.to_s
+#puts a + b
+
+#3 String interpolation
+#a = 45628
+#b = 7839
+
+#puts "The product of 45628 and 7839 is #{a * b}."
+
+
+#4 Value of experession
+#(true and false) = FAlSE; OR TRUE ===> true
+
+(10 < 20 && 30 < 20) || !(10==11)
---
+++ exercise3.rb
@@ -0,0 +1,10 @@
+#3 Outputing the the mathematical results using interpolation.
+print "What is your name?"
+name = gets.chomp
+print "How old are you?"
+age = gets.chomp
+"age".to_i(0)
+year = 2017 - age
+
+puts "Hi #{name}!"
+puts "You were born in #{year}."
