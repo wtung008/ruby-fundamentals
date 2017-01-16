a58aee5ca004ef269a70a5ebb9d564700a6af954
wtung008 <wayne.h.tung@gmail.com>
2017-01-16 17:28:29 -0500

    add ordering of question

--- exercise3.rb
+++ exercise3.rb
@@ -1,9 +1,10 @@
 #3 Outputing the the mathematical results using interpolation.
 print "What is your name?"
 name = gets.chomp
+puts "Hi #{name}!"
+
 print "How old are you?"
 age = gets.chomp.to_i
 year = 2017 - age
 
-puts "Hi #{name}!"
 puts "You were born in #{year}."
